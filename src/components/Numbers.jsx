5ecc69ee728b72e41d5ba384832bdf51215ba39e
Nancy Zitle <nancy.zitle@creditas.com>
2022-08-09 21:18:05 -0500

    feat: refactor Numbers component

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Button from './Button';
 
+const numbers = [1,2,3,4,5,6,7,8,9,0];
+
 const Numbers = ({onClickNumber}) => {
   return (
     <section className='numbers'>
-      <Button text="1" clickHandler={onClickNumber}/>
-      <Button text="2" clickHandler={onClickNumber}/>
-      <Button text="3" clickHandler={onClickNumber}/>
-      <Button text="4" clickHandler={onClickNumber}/>
-      <Button text="5" clickHandler={onClickNumber}/>
-      <Button text="6" clickHandler={onClickNumber}/>
-      <Button text="7" clickHandler={onClickNumber}/>
-      <Button text="8" clickHandler={onClickNumber}/>
-      <Button text="9" clickHandler={onClickNumber}/>
-      <Button text="0" clickHandler={onClickNumber}/>
+    {numbers.map(number => (
+      <Button 
+        key={number} 
+        text={number} 
+        clickHandler={onClickNumber}
+      />
+    ))}
     </section>
   )
 }
