32bd04452fc78fb4673e6670ff1babbafe076d4e
Nancy Zitle <nancy.zitle@creditas.com>
2022-08-10 22:19:22 -0500

    feat: create digit state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import Button from './components/Button';
+import { useState } from 'react';
 import Functions from './components/Functions'
 import MathOperation from './components/MathOperation'
 import Numbers from './components/Numbers';
 import Result from './components/Result'
 import './App.css'
 
-const numbers = () => {
-    console.log('test')
-}
-
 const App = () => {
+    const [digit, setDigit] = useState([]);
+
+    const onClickNumber = (number) => {
+        setDigit([ number])
+    }
+
     return (
-    <main className='react-calculator'>
-        <div className="result">
-            <Result value="0"/>
-        </div>
-        <Numbers onClickNumber={(number) => console.log('number', number)}/>
-        <Functions
-            onContentClear={(operation) => console.log('clear', operation)}
-            onDelete={(operation => console.log('delete', operation))}
-        />
-        <MathOperation
-            onClickOperation={(operation) => console.log('operation', operation)}
-            onClickEqual={(equal) => console.log('equal', equal)}
-        />        
-    </main>)
+        <main className='react-calculator'>
+            <div className="result">
+                <Result value={digit}/>
+            </div>
+            <Numbers onClickNumber={(number) => onClickNumber(number)}/>
+            <Functions
+                onContentClear={(operation) => console.log('clear', operation)}
+                onDelete={(operation => console.log('delete', operation))}
+            />
+            <MathOperation
+                onClickOperation={(operation) => console.log('operation', operation)}
+                onClickEqual={(equal) => console.log('equal', equal)}
+            />        
+        </main>
+    )
 }
 
 export default App
