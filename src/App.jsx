719083e236bb3b1e169776126b0efcf9f6e69645
Nancy Zitle <nancy.zitle@creditas.com>
2022-08-11 23:49:20 -0500

    feat: add lodash and improve our result value deleted the operation value in the string

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 /* eslint no-eval: 0 */
 import { useState } from 'react';
+import words from 'lodash.words';
+
 import Functions from './components/Functions'
 import MathOperation from './components/MathOperation'
 import Numbers from './components/Numbers';
@@ -9,9 +11,17 @@ import './App.css'
 const App = () => {
     const [stack, setStack] = useState("");
 
+    const regexGetOnlyNumbers = /[^-^+^*^/]+/g;
+    const items = words(stack,regexGetOnlyNumbers);
+    const item = items.length ? items[items.length -1] : '0'
+    console.log(item, 'item')
+    // @TODO revisar como esta funcionando
+    // console.log(items, 'itmers')
+    // console.log(items[items.length -1], 'quitando menos 1', items)
     const onDelete = (value) => {
         const length = value.length;
-        if(length > 0) {
+        // length > 0 antes estaba
+        if(length) {
             const newValue = value.substring(0, length - 1);
             setStack(newValue)
         }
@@ -20,11 +30,11 @@ const App = () => {
     return (
         <main className='react-calculator'>
             <div className="result">
-                <Result value={stack}/>
+                <Result value={item}/>
             </div>
             <Numbers onClickNumber={(number) => setStack(`${stack}${number}`)}/>
             <Functions
-                onContentClear={(operation) => console.log(operation)}
+                onContentClear={() => setStack('')}
                 onDelete={( ) => onDelete(stack)}
             />
             <MathOperation
