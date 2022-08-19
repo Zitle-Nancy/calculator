dc29a228da8b308e50c98fae89561bcf6d6af1f4
Nancy Zitle <nancy.zitle@creditas.com>
2022-08-18 21:28:32 -0500

    feat: add firebase dependencies

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const numbers = [1,2,3,4,5,6,7,8,9,0];
+const numbers = [7,8,9,4,5,6,1,2,3,0];
 
 const Numbers = ({onClickNumber}) => {
   return (
